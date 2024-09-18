04ac5f5cd191409a5d9b01debe298c39e8aa4018
Pemba Gelu Sherpa <pembagg122@gmail.com>
2024-09-18 09:03:15 +0545

    Edited

--- mydash/src/pages/Crm.jsx
+++ mydash/src/pages/Crm.jsx
@@ -5,54 +5,46 @@ export default function Crm() {
     <>
       <div className="col-lg-10">
         <div className="mt-4 col-lg-9 float-lg-end col-md-12">
+          {/* --------------------top box pair------------------ */}
           <div className="d-lg-flex">
-            <div class="col-md-12 col-lg-9 mb-3 d-lg-flex ms-lg-4">
+            <div class="col-11 ms-4 col-lg-9 mb-3 mb-lg-0 d-lg-flex ms-lg-2 me-3 me-lg-5">
               <div
-                class="col-lg-5 col-11 bg-white shadow me-3 mb-3"
+                class="col-lg-6 col-12 bg-white shadow me-3 mb-3"
                 id="column1"
               >
                 Box-1
               </div>
-              <div
-                class="col-lg-5 col-11 bg-white shadow ms-3 mb-3"
-                id="column1"
-              >
+              <div class="col-lg-6 col-12 bg-white shadow mb-3" id="column1">
                 Box-2
               </div>
             </div>
             {/* ---------------another box ------------------ */}
-            <div class="d-flex flex-column col-lg-5 ">
-              <div className="box d-flex">
+            <div class="ms-4 ms-lg-0 d-flex flex-column col-lg-5 col-12">
+              <div className="box d-flex col-12">
                 <div
-                  class="col-lg-5 col-md-6 bg-white shadow me-3 mb-3"
+                  class="col-lg-5 col-5 bg-white shadow mb-3 me-3"
                   id="column"
                 >
                   Box-3
                 </div>
-                <div
-                  class="col-lg-5 col-md-4 bg-white shadow ms-3 me-3 mb-3"
-                  id="column"
-                >
+                <div class="col-lg-5 col-5 bg-white shadow mb-3" id="column">
                   Box-4
                 </div>
               </div>
-              <div
-                class="col-lg-11 col-md-4 bg-white shadow me-3 mb-3"
-                id="column"
-              >
+              <div class="col-lg-10 col-10 bg-white shadow mb-3" id="column">
                 Box-5
               </div>
             </div>
           </div>
           {/* ---------another box--------------------- */}
-          <div class="row border mb-3">
+          {/* <div class="row border mb-3">
             <div class="col-6 border ms-3 me-3 mb-3" id="column">
               Box-6
             </div>
             <div class="col-6 border ms-3 me-3 mb-3" id="column">
               Box-7
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
