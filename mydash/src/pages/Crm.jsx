17f25b2e636d1ad141b3a1d2a8787919849a84cd
Pemba Gelu Sherpa <pembagg122@gmail.com>
2024-09-18 15:17:13 +0545

    Edited

--- mydash/src/pages/Crm.jsx
+++ mydash/src/pages/Crm.jsx
@@ -4,10 +4,10 @@ export default function Crm() {
   return (
     <>
       <div className="col-lg-10">
-        <div className="mt-4 col-lg-9 float-lg-end col-md-12">
+        <div className="mt-4 ms-3 col-lg-9 col-12 float-lg-end col-md-12">
           {/* --------------------top box pair------------------ */}
-          <div className="d-lg-flex">
-            <div class="col-11 ms-4 col-lg-9 mb-3 mb-lg-0 d-lg-flex ms-lg-2 me-3 me-lg-5">
+          <div className="d-lg-flex col-lg-10 col-md-12">
+            <div class="col-11 ms-lg-4 me-2 col-lg-9 mb-3 mb-lg-0 d-lg-flex ms-lg-2 me-3 me-lg-5">
               <div
                 class="col-lg-6 col-12 bg-white shadow me-3 mb-3"
                 id="column1"
@@ -19,19 +19,25 @@ export default function Crm() {
               </div>
             </div>
             {/* ---------------another box ------------------ */}
-            <div class="ms-4 ms-lg-0 d-flex flex-column col-lg-5 col-12">
-              <div className="box d-flex col-12">
+            <div class="d-flex flex-column col-lg-7 col-12">
+              <div className="box d-flex col-12 flex-lg-row flex-md-row flex-column">
                 <div
-                  class="col-lg-5 col-5 bg-white shadow mb-3 me-3"
+                  class="col-lg-6 col-md-5 col-11 bg-white shadow mb-3 me-3"
                   id="column"
                 >
                   Box-3
                 </div>
-                <div class="col-lg-5 col-5 bg-white shadow mb-3" id="column">
+                <div
+                  class="col-lg-6 col-md-5 col-11 bg-white shadow mb-3"
+                  id="column"
+                >
                   Box-4
                 </div>
               </div>
-              <div class="col-lg-10 col-10 bg-white shadow mb-3" id="column">
+              <div
+                class="col-lg-12 col-11 bg-white shadow mb-3 ms-lg-2"
+                id="column"
+              >
                 Box-5
               </div>
             </div>
